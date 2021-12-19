d9599aaa9a18863015ed849307cf0992ebbaee14
Joris Griffioen <jorisjgriffioen@gmail.com>
2021-12-19 19:26:23 +0100

    Document why this exists

--- src/addIndexes.ts
+++ src/addIndexes.ts
@@ -1,5 +1,9 @@
 import Bank from "./types/Bank"
 
+/**
+ * The configuration output should have indexes for each bank, preset and message.
+ * We can add them with a simple loop and let the rest of the code forget about them.
+ */
 const addIndexes = (banks: Bank[]): Bank[] =>
   banks.map((bank, bankNumber) => {
     return {
